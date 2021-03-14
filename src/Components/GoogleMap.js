2fd931afac1d7a177268be95248a6dd207e76df4
Nicholas Chung <iamnicholaschung@gmail.com>
2021-03-15 01:51:00 +0800

    refactor to have markers by factory design pattern

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -1,54 +1,56 @@
 import React, { useEffect, useRef } from "react";
 
-const GoogleMap = (userPosition) => {
+const GoogleMap = (positions) => {
+	const allPositions = positions.positions;
+	console.log(allPositions);
 	const googleMapRef = useRef(null);
 	let googleMap;
 	let marker;
+
+	const iconBase = "https://www.flaticon.com/svg/vstatic/svg";
+
+	const icons = {
+		driver: {
+			url: iconBase + "/1048/1048314.svg?token=exp=1615717226~hmac=fd1df196550f266c4f3be3ef3ac3bbcc",
+			scaledSize: new google.maps.Size(30, 30),
+		},
+		user: {
+			url: iconBase + "/3180/3180209.svg?token=exp=1615716990~hmac=4771b645c972ae50d2dcf93468eb5055",
+			scaledSize: new google.maps.Size(30, 30),
+		},
+	};
+
+	const markerTemplate = (type, lat, lng) => {
+		marker = new google.maps.Marker({
+			animation: google.maps.Animation.DROP,
+			position: { lat: lat, lng: lng },
+			icon: icons[type],
+			map: googleMap,
+		});
+	};
+
+	const createMarker = () => {
+		allPositions.forEach((position) => {
+			if (position.type === "user") {
+				markerTemplate(position.type, position.lat, position.lng);
+			} else if (position.type === "driver") {
+				markerTemplate(position.type, position.location.latitude, position.location.longitude);
+			}
+		});
+	};
+
+	const initGoogleMap = () => {
+		return new google.maps.Map(googleMapRef.current, {
+			center: { lat: allPositions[0].lat, lng: allPositions[0].lng },
+			zoom: 14,
+		});
+	};
 	useEffect(() => {
 		googleMap = initGoogleMap();
 		marker = createMarker();
 	}, []);
 
-	const initGoogleMap = () => {
-		return new google.maps.Map(googleMapRef.current, {
-			center: { lat: userPosition.userPosition.lat, lng: userPosition.userPosition.lng },
-			zoom: 16,
-		});
-	};
-
-	const iconBase = "https://www.flaticon.com/svg/vstatic/svg";
-
-	const icons = {
-		parking: {
-			url: iconBase + "/1048/1048314.svg?token=exp=1615717226~hmac=fd1df196550f266c4f3be3ef3ac3bbcc",
-			scaledSize: new google.maps.Size(50, 50),
-		},
-		userLocation: {
-			url: iconBase + "/3180/3180209.svg?token=exp=1615716990~hmac=4771b645c972ae50d2dcf93468eb5055",
-			scaledSize: new google.maps.Size(40, 40),
-		},
-	};
-	const createMarker = () => {
-		marker = new google.maps.Marker({
-			animation: google.maps.Animation.DROP,
-			position: new google.maps.LatLng(userPosition.userPosition),
-			icon: icons.userLocation,
-			map: googleMap,
-		});
-	};
-
-	// const features = [
-	// 	{
-	// 		position: new google.maps.LatLng(userPosition.userPosition.lat, userPosition.userPosition.lng),
-	// 		type: "parking",
-	// 	},
-	// 	{
-	// 		position: new google.maps.LatLng(userPosition.userPosition.lat, userPosition.userPosition.lng),
-	// 		type: "userLocation",
-	// 	},
-	// ];
-
-	return <div ref={googleMapRef} style={{ width: 600, height: 600 }} />;
+	return <div ref={googleMapRef} style={{ width: "90vw", height: "80vh" }} />;
 };
 
 export default GoogleMap;
--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -3,13 +3,14 @@ import Axios from "axios";
 import GoogleMap from "../Components/GoogleMap";
 
 export default function MainPage() {
-	const [positions, setPositions] = useState({});
-	const [loadMap, setLoadMap] = useState(false);
-	const userPosition = {
+	const user = {
 		lat: 51.5049375,
 		lng: -0.0964509,
+		type: "user",
 	};
-	const drivers = 1;
+	const drivers = 10;
+	const [positions, setPositions] = useState([user]);
+	const [loadMap, setLoadMap] = useState(false);
 
 	const loadGoogleMapScript = (callback) => {
 		const googleMapScript = document.createElement("script");
@@ -18,15 +19,6 @@ export default function MainPage() {
 		googleMapScript.addEventListener("load", callback);
 	};
 
-	const loadDriversPositions = () => {
-		useEffect(async () => {
-			const res = await Axios.get(
-				process.env.REACT_APP_BASE_MAP_URL +
-					`/drivers?latitude=${userPosition.lat}&longitude=${userPosition.lng}&count=${drivers}`,
-			);
-			setPositions((positions) => ({ ...positions, drivers: res.data.drivers }));
-		}, []);
-	};
 	const initMap = () => {
 		useEffect(() => {
 			loadGoogleMapScript(() => {
@@ -35,15 +27,21 @@ export default function MainPage() {
 		}, []);
 	};
 
-	const loadUserPosition = () => {
-		useEffect(() => {
-			setPositions((positions) => ({ ...positions, user: userPosition }));
+	const loadDriversPositions = () => {
+		useEffect(async () => {
+			const res = await Axios.get(
+				process.env.REACT_APP_BASE_MAP_URL + `/drivers?latitude=${user.lat}&longitude=${user.lng}&count=${drivers}`,
+			);
+			for (let i = 0; i < res.data.drivers.length; i++) {
+				let driver = res.data.drivers[i];
+				driver["type"] = "driver";
+				setPositions((positions) => [...positions, driver]);
+			}
 		}, []);
 	};
 
 	const initPage = () => {
 		loadDriversPositions();
-		loadUserPosition();
 		initMap();
 	};
 
@@ -51,8 +49,14 @@ export default function MainPage() {
 	return (
 		<div className="mainPage">
 			<h1>{drivers}</h1>
-			<div>{positions?.drivers ? <h2>{positions.drivers[0].driver_id}</h2> : "loading..."} </div>
-			{!loadMap ? <div>Loading...</div> : <GoogleMap userPosition={positions.user} />}
+			{!loadMap ? <div>Loading...</div> : <GoogleMap positions={positions} />}
+			{/* <div>
+				{positions?.drivers
+					? positions.drivers.map((event) => (
+							<p key={event.driver_id}>{`${event.location.latitude}, ${event.location.longitude}`}</p>
+					  ))
+					: "loading..."}{" "}
+			</div> */}
 		</div>
 	);
 }
