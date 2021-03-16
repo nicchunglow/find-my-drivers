f07ceb2e74b26ca0c7abf9029772dfa777bf26ad
Nicholas Chung <iamnicholaschung@gmail.com>
2021-03-16 16:41:13 +0800

    Remove lag from dragging map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -17,8 +17,9 @@ const Map = (props) => {
 		features,
 	};
 
+	let map;
 	useEffect(() => {
-		const map = new mapboxgl.Map({
+		map = new mapboxgl.Map({
 			container: mapContainerRef.current,
 			style: "mapbox://styles/mapbox/streets-v11",
 			center: [lng, lat],
@@ -34,28 +35,14 @@ const Map = (props) => {
 			});
 		});
 		geojson.features.forEach(function (marker) {
-			var el = document.createElement("div");
-			el.className = "marker";
-			if (marker.type === "user") {
-				el.style.backgroundImage = "url(https://placekitten.com/g/" + marker.properties.iconSize.join("/") + "/)";
-			}
-			el.style.width = marker.properties.iconSize[0] + "px";
-			el.style.height = marker.properties.iconSize[1] + "px";
-			el.style.backgroundSize = "100%";
-
 			var popup = new mapboxgl.Popup({ offset: 25 }).setText(marker.properties.message);
-			new mapboxgl.Marker(el)
+			new mapboxgl.Marker()
 				.setLngLat(marker.geometry.coordinates)
 				.setPopup(popup) // sets a popup on this marker
 				.addTo(map);
 		});
 		map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-		map.on("move", () => {
-			setLng(map.getCenter().lng.toFixed(4));
-			setLat(map.getCenter().lat.toFixed(4));
-			setZoom(map.getZoom().toFixed(2));
-		});
 		map.on("mouseenter", "user", function () {
 			map.getCanvas().style.cursor = "pointer";
 		});
@@ -63,9 +50,14 @@ const Map = (props) => {
 		map.on("mouseleave", "user", function () {
 			map.getCanvas().style.cursor = "";
 		});
-		return () => map.remove();
-	}, []);
 
+		map.on("dragend", () => {
+			setLng(map.getCenter().lng.toFixed(4));
+			setLat(map.getCenter().lat.toFixed(4));
+			setZoom(map.getZoom().toFixed(2));
+		});
+		return () => map.remove();
+	}, [geojson]);
 	return (
 		<div>
 			<div>
