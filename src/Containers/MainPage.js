3f0beb4b82a48db064ae118bf4476c05d881475f
Nicholas Chung <iamnicholaschung@gmail.com>
2021-03-22 02:27:47 +0800

    Able to save location

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import { Slider } from "@material-ui/core";
+import { Slider, TextField, Button, Dialog } from "@material-ui/core";
 import "./MainPage.css";
 import Map from "../Components/Map";
 import Loader from "react-loader-spinner";
@@ -20,6 +20,8 @@ export default function MainPage() {
 	const [numOfDrivers, setNumOfDrivers] = useState(2);
 	const [positions, setPositions] = useState([]);
 	const [loading, setloading] = useState(true);
+	const [postSuccess, setpostSuccess] = useState(false);
+	const nameToSave = React.useRef();
 
 	const handleChangeNumOfDrivers = (event, newValue) => {
 		setNumOfDrivers(newValue);
@@ -29,6 +31,31 @@ export default function MainPage() {
 		user.current.geometry.coordinates = [lng, lat];
 		loadPositions();
 	};
+
+	const handleNametoSaveChange = (event) => {
+		console.log(event.target.value);
+		nameToSave.current = event.target.value;
+	};
+
+	const saveUserPosition = async () => {
+		try {
+			if (nameToSave.current.length === 0) {
+				throw new Error("Please fill in name of current location");
+			}
+			let payload = {
+				name: nameToSave.current,
+				coordinates: {
+					lat: user.current.geometry.coordinates[1],
+					lng: user.current.geometry.coordinates[0],
+				},
+			};
+			await Axios.post("https://find-my-drivers-backend.herokuapp.com/locations/create", payload);
+			setpostSuccess(true);
+		} catch (err) {
+			throw new Error(err);
+		}
+	};
+
 	const loadPositions = async () => {
 		let positionArr = [];
 
@@ -65,6 +92,7 @@ export default function MainPage() {
 
 	return (
 		<div className="main-page">
+			{postSuccess === true && <h5> Location Successfully saved! </h5>}
 			<h2>FIND MY DRIVERS</h2>
 			{!!loading && <Loader className="loader" type="TailSpin" color="#00BFFF" height={40} width={40} />}
 			<div className="main-page-container ">
@@ -77,6 +105,16 @@ export default function MainPage() {
 				)}
 				<div className="slider-container">
 					<h4>Double click on the map to change user location!</h4>
+					<TextField
+						id="outlined-basic"
+						label="Name and save your current location"
+						variant="outlined"
+						fullWidth
+						onChange={(event) => handleNametoSaveChange(event)}
+					/>
+					<Button variant="contained" color="secondary" onClick={() => saveUserPosition()}>
+						Save
+					</Button>
 					<h4>Current number of drivers: {numOfDrivers}</h4>
 					<div className="slider">
 						<Slider
